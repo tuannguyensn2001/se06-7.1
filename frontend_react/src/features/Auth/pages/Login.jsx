7603082c5e13b34ddfeee29e9578d293cbaba6d0
unknown <phampham19102001@gmail.com>
2021-11-22 15:20:10 +0700

    UI login and signup

--- frontend_react/src/features/Auth/pages/Login.jsx
+++ frontend_react/src/features/Auth/pages/Login.jsx
@@ -1,14 +1,27 @@
-import styled from "styled-components";
-
-const Title = styled.div`
-  color: blue;
-  background-color: red;
-`;
-
-//PascalCase
 
 function Login() {
-  return <Title>Login</Title>;
+  return (
+    <div class="login-box">
+      <h2>Log in</h2>
+      <form>
+        <div class="user-box">
+          <input type="text" name="" required=""></input>
+          <label>Username</label>
+        </div>
+        <div class="user-box">
+          <input type="password" name="" required=""></input>
+          <label>Password</label>
+        </div>
+        <a href="#">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Submit
+        </a>
+      </form>
+    </div>
+  )
 }
 
 export default Login;
--- frontend_react/src/routes.js
+++ frontend_react/src/routes.js
@@ -1,6 +1,7 @@
 import Viewer from "@/features/Viewer";
 import Home from "@/features/Home";
 import Login from "@/features/Auth/pages/Login";
+import Register from "./features/Auth/pages/Register";
 
 const routes = [
   {
@@ -14,6 +15,10 @@ const routes = [
   {
     path: "/login",
     component: <Login />,
+  },
+  {
+    path: "/register",
+    component: <Register />,
   },
 ];
 
