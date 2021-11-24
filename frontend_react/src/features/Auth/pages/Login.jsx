419641e2a51792621e094842f4afa3ca3f2a4dbf
unknown <phampham19102001@gmail.com>
2021-11-24 20:40:13 +0700

    .

--- frontend_react/src/features/Auth/pages/Login.jsx
+++ frontend_react/src/features/Auth/pages/Login.jsx
@@ -1,30 +1,4 @@
 
-
-// function Login() {
-//   return (
-//     <div class="login-box">
-//       <h2>Log in</h2>
-//       <form>
-//         <div class="user-box">
-//           <input type="text" name="" required=""></input>
-//           <label>Username</label>
-//         </div>
-//         <div class="user-box">
-//           <input type="password" name="" required=""></input>
-//           <label>Password</label>
-//         </div>
-//         <a href="#">
-//           <span></span>
-//           <span></span>
-//           <span></span>
-//           <span></span>
-//           Submit
-//         </a>
-//       </form>
-//     </div>
-//   )
-
-
 import useAuth from "@/hooks/useAuth";
 import { useEffect } from "react";
 
@@ -43,6 +17,27 @@ function Login() {
       >
         Click
       </button>
+
+      <div class="login-box">
+      <h2>Log in</h2>
+      <form>
+        <div class="user-box">
+          <input type="text" name="" required=""></input>
+          <label>Username</label>
+        </div>
+        <div class="user-box">
+          <input type="password" name="" required=""></input>
+          <label>Password</label>
+        </div>
+        <a href="#">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Submit
+        </a>
+      </form>
+    </div>
     </div>
   );
 
