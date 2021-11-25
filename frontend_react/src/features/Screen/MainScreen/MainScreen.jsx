4d41c80ab1e8f60782bebce9e93f715bb30571b0
Vinh <inhden687@gmail.com>
2021-11-25 16:28:10 +0700

    demo lan 1 UI man chinh

--- frontend_react/src/features/Screen/MainScreen/MainScreen.jsx
+++ frontend_react/src/features/Screen/MainScreen/MainScreen.jsx
@@ -24,6 +24,69 @@ export default function MainScreen() {
                         <div className="search"></div>
                     </div>
                 </div>
+                <div id="content-container" >
+                    <div className=" grid grid-cols-3 gap-20 p-20">
+                        <div className="content-list w-60 h-60 mb-20">
+                            <img src={avatar}/>
+                            <p className="content-text w-full text-center pt-10 pb-5 ">Sieu nhan</p>
+                        </div>
+                        <div className="content-list w-60 h-60 mb-20">
+                            <img src={avatar}/>
+                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+                        </div>
+                        <div className="content-list w-60 h-60 mb-20">
+                            <img src={avatar}/>
+                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+                        </div>
+                        <div className="content-list w-60 h-60 mb-20">
+                            <img src={avatar}/>
+                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+                        </div>
+                        <div className="content-list w-60 h-60 mb-20">
+                            <img src={avatar}/>
+                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+                        </div>
+                        <div className="content-list w-60 h-60 mb-20">
+                            <img src={avatar}/>
+                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <footer class="footer">
+                    <div class="waves">
+                    <div class="wave" id="wave1"></div>
+                    <div class="wave" id="wave2"></div>
+                    <div class="wave" id="wave3"></div>
+                    <div class="wave" id="wave4"></div>
+                    </div>
+                    <ul class="social-icon">
+                    <li class="social-icon__item"><a class="social-icon__link" href="#">
+                        <ion-icon name="logo-facebook"></ion-icon>
+                        </a></li>
+                    <li class="social-icon__item"><a class="social-icon__link" href="#">
+                        <ion-icon name="logo-twitter"></ion-icon>
+                        </a></li>
+                    <li class="social-icon__item"><a class="social-icon__link" href="#">
+                        <ion-icon name="logo-linkedin"></ion-icon>
+                        </a></li>
+                    <li class="social-icon__item"><a class="social-icon__link" href="#">
+                        <ion-icon name="logo-instagram"></ion-icon>
+                        </a></li>
+                    </ul>
+                    <ul class="menu">
+                    <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
+                    <li class="menu__item"><a class="menu__link" href="#">About</a></li>
+                    <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
+                    <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
+                    <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
+
+                    </ul>
+                    <p>&copy;2021 SE06-7.1 | All Rights Reserved</p>
+                </footer>
+
             </div>
         </div>
     )
