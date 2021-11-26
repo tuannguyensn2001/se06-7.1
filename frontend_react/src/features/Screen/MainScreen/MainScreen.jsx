bc963266b434af530740a657c6fe476ef3541885
Vinh <inhden687@gmail.com>
2021-11-26 15:22:48 +0700

    them animation

--- frontend_react/src/features/Screen/MainScreen/MainScreen.jsx
+++ frontend_react/src/features/Screen/MainScreen/MainScreen.jsx
@@ -26,62 +26,100 @@ export default function MainScreen() {
                 </div>
                 <div id="content-container" >
                     <div className=" grid grid-cols-3 gap-20 p-20">
-                        <div className="content-list w-60 h-60 mb-20">
-                            <img src={avatar}/>
-                            <p className="content-text w-full text-center pt-10 pb-5 ">Sieu nhan</p>
+                        <div className="content-list w-64 h-60 mb-20">
+                            <div className="content-item ">
+                                <img src={avatar}/>
+                                <div className="content-content">
+                                    <h1 className="content-title w-full pl-24 pb-2 pt-4 ">Sieu nhan</h1>
+                                    <div className="button w-full text-center pb-10 pt-5 text-2xl "><a href="#">Go to Editor</a></div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="content-list w-60 h-60 mb-20">
-                            <img src={avatar}/>
-                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+
+                        <div className="content-list w-64 h-60 mb-20">
+                            <div className="content-item ">
+                                <img src={avatar}/>
+                                <div className="content-content">
+                                    <h1 className="content-title w-full pl-24 pb-2 pt-4 ">Sieu nhan</h1>
+                                    <div className="button w-full text-center pb-10 pt-5 text-2xl"><a href="#">Go to Editor</a></div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="content-list w-60 h-60 mb-20">
-                            <img src={avatar}/>
-                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+
+                        <div className="content-list w-64 h-60 mb-20">
+                            <div className="content-item ">
+                                <img src={avatar}/>
+                                <div className="content-content">
+                                    <h1 className="content-title w-full pl-24 pb-2 pt-4 ">Sieu nhan</h1>
+                                    <div className="button w-full text-center pb-10 pt-5 text-2xl"><a href="#">Go to Editor</a></div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="content-list w-60 h-60 mb-20">
-                            <img src={avatar}/>
-                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+
+                        <div className="content-list w-64 h-60 mb-20">
+                            <div className="content-item ">
+                                <img src={avatar}/>
+                                <div className="content-content">
+                                    <h1 className="content-title w-full pl-24 pb-2 pt-4 ">Sieu nhan</h1>
+                                    <div className="button w-full text-center pb-10 pt-5 text-2xl"><a href="#">Go to Editor</a></div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="content-list w-60 h-60 mb-20">
-                            <img src={avatar}/>
-                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+
+                        <div className="content-list w-64 h-60 mb-20">
+                            <div className="content-item ">
+                                <img src={avatar}/>
+                                <div className="content-content">
+                                    <h1 className="content-title w-full pl-24 pb-2 pt-4 ">Sieu nhan</h1>
+                                    <div className="button w-full text-center pb-10 pt-5 text-2xl"><a href="#">Go to Editor</a></div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="content-list w-60 h-60 mb-20">
-                            <img src={avatar}/>
-                            <p className="content-text w-full text-center pt-10 pb-5">Sieu nhan</p>
+
+                        <div className="content-list w-64 h-60 mb-20">
+                            <div className="content-item ">
+                                <img src={avatar}/>
+                                <div className="content-content">
+                                    <h1 className="content-title w-full pl-24 pb-2 pt-4 ">Sieu nhan</h1>
+                                    <div className="button w-full text-center pb-10 pt-5 text-2xl"><a href="#">Go to Editor</a></div>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
+
+
 
                 </div>
 
 
-                <footer class="footer">
-                    <div class="waves">
-                    <div class="wave" id="wave1"></div>
-                    <div class="wave" id="wave2"></div>
-                    <div class="wave" id="wave3"></div>
-                    <div class="wave" id="wave4"></div>
+                <footer className="footer">
+                    <div className="waves">
+                    <div className="wave" id="wave1"></div>
+                    <div className="wave" id="wave2"></div>
+                    <div className="wave" id="wave3"></div>
+                    <div className="wave" id="wave4"></div>
                     </div>
-                    <ul class="social-icon">
-                    <li class="social-icon__item"><a class="social-icon__link" href="#">
+                    <ul className="social-icon">
+                    <li className="social-icon__item"><a className="social-icon__link" href="#">
                         <ion-icon name="logo-facebook"></ion-icon>
                         </a></li>
-                    <li class="social-icon__item"><a class="social-icon__link" href="#">
+                    <li className="social-icon__item"><a className="social-icon__link" href="#">
                         <ion-icon name="logo-twitter"></ion-icon>
                         </a></li>
-                    <li class="social-icon__item"><a class="social-icon__link" href="#">
+                    <li className="social-icon__item"><a className="social-icon__link" href="#">
                         <ion-icon name="logo-linkedin"></ion-icon>
                         </a></li>
-                    <li class="social-icon__item"><a class="social-icon__link" href="#">
+                    <li className="social-icon__item"><a className="social-icon__link" href="#">
                         <ion-icon name="logo-instagram"></ion-icon>
                         </a></li>
                     </ul>
-                    <ul class="menu">
-                    <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
-                    <li class="menu__item"><a class="menu__link" href="#">About</a></li>
-                    <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
-                    <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
-                    <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
+                    <ul className="menu">
+                    <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
+                    <li className="menu__item"><a className="menu__link" href="#">About</a></li>
+                    <li className="menu__item"><a className="menu__link" href="#">Services</a></li>
+                    <li className="menu__item"><a className="menu__link" href="#">Team</a></li>
+                    <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
 
                     </ul>
                     <p>&copy;2021 SE06-7.1 | All Rights Reserved</p>
