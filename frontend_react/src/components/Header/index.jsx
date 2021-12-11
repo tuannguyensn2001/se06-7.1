cd90762ff5180bd0ede26cd72e9ade80b1df11c7
Vinh <inhden687@gmail.com>
2021-12-11 22:35:36 +0700

    phoi lai mau phan header

--- frontend_react/src/components/Header/index.jsx
+++ frontend_react/src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import useAuth from "@/hooks/useAuth";
 import styles from "./style.module.scss";
 import { Link } from "react-router-dom";
 import avatar from "../../assets/img/man.png"
-import like from "../../assets/img/like.png"
+import like from "../../assets/img/apple.png"
 import { Menu, Dropdown } from 'antd';
 import { useState } from "react";
 
@@ -22,8 +22,8 @@ function Header() {
   return (
     <div className={styles.header}>
       <div className="flex space-x">
-        <div className="h-full"><img className="h-5/6 pt-2 pl-2" src={like}/></div>
-        <h1 className={` ${styles["title"]} ${"text-white m-3 font-bold text-lg font-serif"}`}>Se06-7.1</h1>
+
+        <h1 className={` ${styles["nav-item"]} ${"text-white m-3 font-bold text-lg "}`}>Se06-7.1</h1>
       </div>
 
       {/* Chỗ này là khi mà ở màn chưa đăng nhập dùng để căn chỉnh home và go to model ra giữa */}
@@ -31,8 +31,8 @@ function Header() {
       {
         isAuth && 
         <div className="flex space-x-4 m-3 space-between">
-          <Link to={"/"} className={` ${styles["nav-item"]} ${"text-white"} `}>Home</Link>
-          <button className={`${styles["nav-item"]} ${"text-white hover:text-blue-500 rounded hover:bg-white transition-colors duration-300"} `}>Go to my model</button>
+          <Link to={"/"} className={` ${styles["nav-item"]} ${"text-white ml-20"} `}>Home</Link>
+          <button className={`${styles["nav-item"]} ${"text-white mr-2"} `}>Go to my model</button>
         </div>
       }
 
@@ -49,7 +49,7 @@ function Header() {
         <div className="flex space-x-4 m-3 space-between ">
           <Link className={`${styles["nav-item"]} ${"text-white"}`} to={"/login"}>Đăng nhập</Link>
 
-          <Link className={`${styles["nav-item"]} ${"text-blue-700 border border-solid bg-white rounded hover:bg-gray-100 hover:text-blue-500 transition-colors duration-300 "}`} to={"/register"}>Register</Link>
+          <Link className={`${styles["nav-item"]} ${"text-blue-700 border border-solid bg-white rounded hover:bg-gray-100 hover:text-blue-500 transition-colors duration-300 "}`} to={"/register"}>Đăng kí</Link>
 
         </div>
       }
@@ -71,11 +71,11 @@ function Header() {
 
               <Menu className={`${"rounded z-10 h-60"} ${checked ? "visible" : "invisible"}`} >
                   <Menu.Item>
-                    <button className="w-32 h-full ">Setting</button>
+                    <button className="w-28 h-full ">Setting</button>
                   </Menu.Item>
               
                   <Menu.Item>
-                    <button className="w-32 h-full " onClick={logoutMutation.mutate}>Đăng xuất</button>
+                    <button className="w-28 h-full " onClick={logoutMutation.mutate}>Đăng xuất</button>
                   </Menu.Item>
               </Menu>
             </div>
